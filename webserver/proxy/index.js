a455c7642bf393ed89b1058ae3814b852d56d976
Bjorge Staijen <bstaijen@gmail.com>
2017-01-05 12:19:52 -0800

    proxy now uses environment variables.

--- webserver/proxy/index.js
+++ webserver/proxy/index.js
@@ -1,16 +1,22 @@
 var path = require("path");
 var fs = require("fs");
+var env = require('node-env-file');
+
+// Does not overwrite existing environment variables.
+if (fs.existsSync(__dirname + '/.env')) {
+    env(__dirname + '/.env');
+    console.log(env.data)
+}
 var express = require('express');
 var morgan = require("morgan");
 var app      = express();
 var httpProxy = require('http-proxy');
 var apiProxy = httpProxy.createProxyServer();
-var profileService = 'http://profile:5000',
-    authenticationService = 'http://authentication:5001',
-    photoService = 'http://photo:5002',
-    voteService = 'http://vote:5003',
-    commentService = 'http://comment:5004';
-
+var profileService =  process.env.PROFILE_URL,
+    authenticationService =  process.env.AUTHENTICATION_URL,
+    photoService =  process.env.PHOTO_URL,
+    voteService =  process.env.VOTE_URL,
+    commentService =  process.env.COMMENT_URL;
 
 var accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a'});
 app.use(morgan('combined', {stream: accessLogStream}));
@@ -42,7 +48,6 @@ app.all("/comments*", function(req, res) {
     console.log('redirecting to commentService');
     apiProxy.web(req, res, {target: commentService});
 });
-
-app.listen(4999, function () {
-    console.log('Example app listening on port 4999!')
+app.listen(process.env.PORT, function () {
+    console.log('Proxy listening on port %s!', process.env.PORT)
 });
