f8b052c3f2cf933c5f3a16b2ce26f3590b86939b
Bjorge Staijen <bstaijen@gmail.com>
2017-04-26 13:03:49 -0700

    Removed some things from create_machines.sh script

--- scripts/create_machines.sh
+++ scripts/create_machines.sh
@@ -16,9 +16,7 @@ echo "---Swarm Init"
 docker-machine ssh manager1 docker swarm init --listen-addr ${manager_ip} --advertise-addr ${manager_ip}
 
 printf "\n---Get Tokens\n"
-manager_token=$(docker-machine ssh manager1 docker swarm join-token -q manager)
 worker_token=$(docker-machine ssh manager1 docker swarm join-token -q worker)
-echo ${manager_token}
 echo ${worker_token}
 
 for n in {1..4} ; do
@@ -28,28 +26,3 @@ for n in {1..4} ; do
 	echo "--- Swarm Worker Join"
 	docker-machine ssh worker${n} docker swarm join --listen-addr ${ip} --advertise-addr ${ip} --token ${worker_token} ${manager_ip}:2377
 done
-
-printf "\n---Launching Visualizer\n"
-docker-machine ssh manager1 docker run -it -d -p 8080:8080 -e HOST=${manager_ip} -e PORT=8080 -v /var/run/docker.sock:/var/run/docker.sock manomarks/visualizer
-
-
-printf "\n---Launching ETCD\n"
-eval $(docker-machine env manager1)
-docker run -d -v /usr/share/ca-certificates/:/etc/ssl/certs -p 4001:4001 -p 2380:2380 -p 2379:2379 \
- --name etcd quay.io/coreos/etcd etcd\
- -name etcd0 \
- -advertise-client-urls http://${manager_ip}:2379,http://${manager_ip}:4001 \
- -listen-client-urls http://0.0.0.0:2379,http://0.0.0.0:4001 \
- -initial-advertise-peer-urls http://${manager_ip}:2380 \
- -listen-peer-urls http://0.0.0.0:2380 \
- -initial-cluster-token etcd-cluster-1 \
- -initial-cluster etcd0=http://${manager_ip}:2380 \
- -initial-cluster-state new
-
-docker network create --driver overlay --opt encrypted my-network
-
-printf "\n\n------------------------------------\n"
-echo "To visualize your cluster..."
-echo "Open a browser to http://${manager_ip}:8080/"
-echo "To connect to your cluster..."
-echo 'eval $(docker-machine env manager1)'
